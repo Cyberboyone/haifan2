31dbe2df8d4cffe25fe468e8b732ac5342123b07
Cyberboyone <cyberboyone@users.noreply.github.com>
2026-09-15 14:58:21 +0100

    Initial commit - Haifan Shuabul Iman 2

---
+++ lib/data/sample_lessons.dart
@@ -0,0 +1,129 @@
+import '../models/lesson.dart';
+
+const List<Lesson> sampleLessons = [
+  Lesson(
+    title: "025 Shu'abul Iman - Girmama Al-Qur'ani (3)",
+    audioPath: "assets/audio/Shu'abul Iman 025 Girmama Al-Qur'ani (3).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "026 Shu'abul Iman - Muhimmancin Tsarki",
+    audioPath: "assets/audio/Shu'abul Iman 026 Muhimmancin Tsarki.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "027 Shu'abul Iman - Kafircin Barin Salla",
+    audioPath: "assets/audio/Shu'abul Iman 027 Kafircin Barin Salla.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "028 Shu'abul Iman - Matsayin Zakka",
+    audioPath: "assets/audio/Shu'abul Iman 028 Matsayin Zakka.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "029 Shu'abul Iman - Azabar Kin Zakka",
+    audioPath: "assets/audio/Shu'abul Iman 029 Azabar 'Kin Zakka.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "030 Shu'abul Iman - Kimar Azumi",
+    audioPath: "assets/audio/Shu'abul Iman 030 Kimar Azumi.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "031 Shu'abul Iman - Al-I'itikaf (1)",
+    audioPath: "assets/audio/Shu'abul Iman 031 Al-I'itikaf (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "032 Shu'abul Iman - Al-I'itikaf (2)",
+    audioPath: "assets/audio/Shu'abul Iman 032 Al-I'itikaf (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "033 Shu'abul Iman - Tafiyar Hajji (1)",
+    audioPath: "assets/audio/Shu'abul Iman 033 Tafiyar Hajji (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "034 Shu'abul Iman - Tafiyar Hajji (2)",
+    audioPath: "assets/audio/Shu'abul Iman 034 Tafiyar Hajji (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "035 Shu'abul Iman - Jihadi A Islam (1)",
+    audioPath: "assets/audio/Shu'abul Iman 035 Jihadi A Islam (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "036 Shu'abul Iman - Jihadi A Islam (2)",
+    audioPath: "assets/audio/Shu'abul Iman 036 Jihadi A Islam (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "037 Shu'abul Iman - Falalar Jihadi",
+    audioPath: "assets/audio/Shu'abul Iman 037 Falalar Jihadi.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "038 Shu'abul Iman - Tsaron Iyakokin Musulmai",
+    audioPath: "assets/audio/Shu'abul Iman 038 Tsaron Iyakokin Musulmai.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "039 Shu'abul Iman - Tirjewa A Ya'ki (1)",
+    audioPath: "assets/audio/Shu'abul Iman 039 Tirjewa A Ya'ki (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "040 Shu'abul Iman - Tirjewa A Ya'ki (2)",
+    audioPath: "assets/audio/Shu'abul Iman 040 Tirjewa A Ya'ki (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "041 Shu'abul Iman - Bin Tsarin Ganima (1)",
+    audioPath: "assets/audio/Shu'abul Iman 041 Bin Tsarin Ganima (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "042 Shu'abul Iman - Bin Tsarin Ganima (2)",
+    audioPath: "assets/audio/Shu'abul Iman 042 Bin Tsarin Ganima (2).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "043 Shu'abul Iman - 'Yantar Da Bayi",
+    audioPath: "assets/audio/Shu'abul Iman 043 'Yantar Da Bayi.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "044 Shu'abul Iman - Gudanar Da Kaffara (1)",
+    audioPath: "assets/audio/Shu'abul Iman 044 Gudanar Da Kaffara (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "045 Shu'abul Iman - Tsare Kowace Doka (1)",
+    audioPath: "assets/audio/Shu'abul Iman 045 Tsare Kowace Doka (1).ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "046 Shu'abul Iman - Ukubar Sa'ba Alkawari",
+    audioPath: "assets/audio/Shu'abul Iman 046 Ukubar Sa'ba Alkawari.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "047 Shu'abul Iman - Kiyaye Yarjejeniya",
+    audioPath: "assets/audio/Shu'abul Iman 047 Kiyaye Yarjejeniya.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "048 Shu'abul Iman - Gaskiya Ta Mu'amala",
+    audioPath: "assets/audio/Shu'abul Iman 048 Gaskiya Ta Mu'amala.ogg",
+    course: "Shu'abul Iman",
+  ),
+  Lesson(
+    title: "049 Shu'abul Iman - Cika Kowane Sharadi",
+    audioPath: "assets/audio/Shu'abul Iman 049 Cika Kowane Sharadi.ogg",
+    course: "Shu'abul Iman",
+  ),
+];
